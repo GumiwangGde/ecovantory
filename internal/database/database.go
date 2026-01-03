b2aadd3627507d2e0447fcfca7f01455d7d0b0c2
Gde <gugumgde26@gmail.com>
2026-01-03 19:46:54 +0700

    feat: setup sqlite database connection and auto-migration for all models

---
+++ internal/database/database.go
@@ -0,0 +1,39 @@
+package database
+
+import (
+	"ecovantory/internal/models"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"log"
+)
+
+var DB *gorm.DB
+
+func InitDB() {
+	var err error
+
+	DB, err = gorm.Open(sqlite.Open("ecovantory.db"), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("Failed to connect to database: ", err)
+	}
+
+	err = DB.AutoMigrate(
+		&models.User{},
+		&models.Category{},
+		&models.Unit{},
+		&models.Item{},
+		&models.ItemCategory{},
+		&models.Stock{},
+		&models.StockMutations{},
+		&models.Transaction{},
+		&models.TransactionDetail{},
+		&models.Log{},
+	)
+
+	if err != nil {
+		log.Fatal("Failed to migrate to database: ", err)
+	}
+
+	log.Println("Database successfully initialized and migrated")
+}
